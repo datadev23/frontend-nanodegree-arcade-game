c720ef6c0eabf099263deed7efe5c34572f106d7
maindev <systemdevcloud@live.co.uk>
2016-05-25 22:13:04 +0100

    change to key down

--- js/app.js
+++ js/app.js
@@ -120,7 +120,7 @@ var player = new Player(10,400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
