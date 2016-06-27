055845c9ddc883c0bed8a4d79edb651aaef38a4d
devel <systemdevcloud@live.co.uk>
2016-06-27 15:09:24 +0100

    fixed speed calculation

--- js/app.js
+++ js/app.js
@@ -27,7 +27,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-   this.x += this.x +1 *dt;
+   this.x += this.x * 0.001;
+
+   console.log(this.x);
     //this.x*dt;
     this.y*dt;
 
